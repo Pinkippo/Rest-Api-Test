42defcdc4ead72b92a77a4270ef0133256d1ac88
Seungwan Han <hseungwan@naver.com>
2023-01-04 18:41:32 +0900

    환경설정

---
+++ src/main/java/com/example/restapitest/RestApiTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.restapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTestApplication.class, args);
+    }
+
+}
